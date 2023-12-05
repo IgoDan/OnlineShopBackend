7956dfcee4f18fbd77f66afa41d65d5a0b8c5140
IgoDan <mr.igor.daniluk@gmail.com>
2023-12-05 15:49:50 +0100

    Delete async Redis connection

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(redis());
 
 // Tworzenie klienta Redis
 const redisClient = redis.createClient({
-  host: 'redis://red-clni7ipll56s73ficld0',
-  port: '6379'
+  host: 'red-clni7ipll56s73ficld0',
+  port: 6379
 });
 
 redisClient.on('error', (err) => {
@@ -34,7 +33,7 @@ app.get("/products", (req, res) => {
     res.send(products);
 });
 
-app.get('/orders', async (req, res) => {
+app.get('/orders', (req, res) => {
     try {
         res.json(orders);
     } catch (error) {
@@ -43,31 +42,37 @@ app.get('/orders', async (req, res) => {
     }
 });
 
-app.post('/orders', async (req, res) => {
+app.post('/orders', (req, res) => {
     try {
         const newOrder = req.body;
         orders.push(newOrder);
 
         // Zapisywanie zamówień w pliku
         const updatedOrdersFile = JSON.stringify(orders, null, 2);
-        await fs.writeFile('orders.js', `module.exports = ${updatedOrdersFile};`, 'utf-8');
-
-        // Pobieranie indeksu zamówienia
-        const orderIndex = await new Promise((resolve, reject) => {
+        fs.writeFile('orders.js', `module.exports = ${updatedOrdersFile};`, 'utf-8')
+          .then(() => {
+            // Pobieranie indeksu zamówienia
             redisClient.incr('orderIndex', (err, orderIndex) => {
                 if (err) {
                     console.error(err);
-                    reject(err);
+                    res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia w Redis' });
                 } else {
-                    resolve(orderIndex);
+                    // Zapisywanie zamówienia w Redis
+                    redisClient.set(`order:${orderIndex}`, JSON.stringify(newOrder), (err, result) => {
+                        if (err) {
+                            console.error(err);
+                            res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia w Redis' });
+                        } else {
+                            res.json({ message: 'Zamówienie dodane pomyślnie' });
+                        }
+                    });
                 }
             });
-        });
-
-        // Zapisywanie zamówienia w Redis
-        redisClient.set(`order:${orderIndex}`, JSON.stringify(newOrder));
-
-        res.json({ message: 'Zamówienie dodane pomyślnie' });
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
+          });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
