84b23a319a62ef10a2eae9fea9a7c64a4197281b
IgoDan <mr.igor.daniluk@gmail.com>
2023-12-05 14:49:01 +0100

    Redis fix

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const fs = require('fs/promises');
 const redis = require('redis');
 
-const products = require('./products');
-var orders = require('./orders');
+const products = require("./products");
+const orders = require("./orders");
 
 const app = express();
+const redisClient = redis.createClient(process.env.REDIS_URL || 'redis://red-clni7ipll56s73ficld0:6379');
 
 app.use(express.json());
 app.use(cors());
 
-const client = redis.createClient({
-  url: 'redis://red-clni7ipll56s73ficld0:6379',
+app.get("/", (req, res) => {
+    res.send("Witam w API sklepu");
 });
 
-client.on('error', (err) => {
-  console.error(`Redis Error: ${err}`);
-});
-
-app.get('/', (req, res) => {
-  res.send('Witam w API sklepu');
-});
-
-app.get('/products', (req, res) => {
-  res.send(products);
+app.get("/products", (req, res) => {
+    res.send(products);
 });
 
 app.get('/orders', async (req, res) => {
-  try {
-    res.json(orders);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Błąd podczas odczytu zamówień' });
-  }
+    try {
+        res.json(orders);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Błąd podczas odczytu zamówień' });
+    }
 });
 
 app.post('/orders', async (req, res) => {
-  try {
-    const newOrder = req.body;
-    orders.push(newOrder);
+    try {
+        const newOrder = req.body;
+        orders.push(newOrder);
 
-    const updatedOrders = JSON.stringify(orders, null, 2);
-    await fs.writeFile('orders.js', `module.exports = ${updatedOrders};`, 'utf-8');
+        // Zapisywanie zamówień w pliku
+        const updatedOrdersFile = JSON.stringify(orders, null, 2);
+        await fs.writeFile('orders.js', `module.exports = ${updatedOrdersFile};`, 'utf-8');
 
-    // Dodawanie zamówienia do Redis z autoinkrementowanym indeksem
-    const orderId = await client.incr('orderIndex');
-    await client.set(`order:${orderId}`, JSON.stringify(newOrder));
+        // Dodawanie zamówienia do Redis
+        redisClient.incr('orderIndex', (err, orderIndex) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
+            }
 
-    res.json({ message: 'Zamówienie dodane pomyślnie' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
-  }
+            redisClient.set(`order:${orderIndex}`, JSON.stringify(newOrder), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
+                }
+
+                res.json({ message: 'Zamówienie dodane pomyślnie' });
+            });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
+    }
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Serwer działa na porcie ${port}`);
+    console.log(`Serwer działa na porcie ${port}`);
 });
