fc950b41bfc1acc36430a2205e0ef612de09226c
IgoDan <mr.igor.daniluk@gmail.com>
2023-12-06 17:14:42 +0100

    Add Redis commands

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
-const express = require("express");
-const cors = require("cors");
-const fs = require('fs/promises');
+import express from 'express';
+import cors from 'cors';
+import { createClient } from 'redis';
 
-const products = require("./products")
-var orders = require("./orders")
+const products = require('./products');
+const orders = require('./orders');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Witam w API sklepu");
+const redisClient = createClient({
+  url: process.env.REDIS_URL || 'redis://red-clni7ipll56s73ficld0:6379',
 });
 
-app.get("/products", (req, res) => {
-    res.send(products);
+client.on('connect', () => {
+  console.log('Połączono z Redis');
+});
+
+client.on('error', (err) => {
+  console.error(`Błąd połączenia z Redis: ${err}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Witam w API sklepu');
+});
+
+app.get('/products', (req, res) => {
+  res.send(products);
 });
 
 app.get('/orders', async (req, res) => {
-    try {
-      res.json(orders);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Błąd podczas odczytu zamówień' });
-    }
-  });
-  
-  app.post('/orders', async (req, res) => {
-    try {
-      const newOrder = req.body;
-      orders.push(newOrder);
-  
-      const updatedOrders = JSON.stringify(orders, null, 2);
-      await fs.writeFile('orders.js', `module.exports = ${updatedOrders};`, 'utf-8');
-  
-      res.json({ message: 'Zamówienie dodane pomyślnie' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
-    }
-  });
+  try {
+    const ordersFromRedis = await redisClient.hgetall('orders');
+
+    const ordersArray = Object.values(ordersFromRedis).map((order) =>
+      JSON.parse(order)
+    );
+
+    res.json(ordersArray);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Błąd podczas odczytu zamówień' });
+  }
+});
+
+app.post('/orders', async (req, res) => {
+  try {
+    const newOrder = req.body;
+
+    const currentIndex = await redisClient.incr('orderIndex');
+
+    await redisClient.hset('orders', currentIndex, JSON.stringify(newOrder));
+
+    res.json({ message: 'Zamówienie dodane pomyślnie' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Błąd podczas zapisywania zamówienia' });
+  }
+});
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`Serwer dziala na porcie ${port}`));
+app.listen(port, () => {
+  console.log(`Serwer działa na porcie ${port}`);
+});
